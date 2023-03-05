92fd215facc224969ed8233c59d769d4c099b682
Juan <juanvegat@unisangil.edu.co>
2023-03-05 17:32:58 -0500

    archivo que resta dos numeros

---
+++ resta.py
@@ -0,0 +1,7 @@
+# Este archivo resta dos numeros pedidos por consola
+
+num1 = int(input("Igrese el primer numero: "))
+num2 = int(input("Igrese el segundo numero: "))
+
+print(f"La suma es: {num1 - num2}")
+8
